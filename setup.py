260a515011bc3c0fcb517e4ad8a40f04ceaaf5e6
zom <t.fontana@net2action.com>
2020-02-23 15:46:30 +0100

    fixed travis and added dependancies

--- setup.py
+++ setup.py
@@ -59,6 +59,10 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     tests_require=test_deps,
     # Add here the package dependencies
-    install_requires=[],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "compress_pickle"
+    ],
     extras_require=extras,
 )
