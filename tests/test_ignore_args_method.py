595d128698cc27cd668b3410b5b24173505f4bf7
zom <t.fontana@net2action.com>
2020-02-24 10:08:37 +0100

    added test, hopefully it should have 100% coverage

--- tests/test_ignore_args_method.py
+++ tests/test_ignore_args_method.py
@@ -2,27 +2,32 @@ from shutil import rmtree
 from time import sleep, perf_counter
 from cache_decorator import cache
 
-@cache(
-    cache_path="{cache_dir}/{_hash}.pkl.lzma",
-    cache_dir="./test_cache"
-)
-def cached_function(a):
-    sleep(2)
-    return [1, 2, 3]
+class A:
+    def __init__(self):
+        pass
+    
+    @cache(
+        cache_dir="./test_cache",
+        args_to_ignore=["x"],
+        )
+    def cached_function(self, a, x):
+        sleep(2)
+        return [1, 2, 3]
 
 
-def test_pickle_lzma():
+def test_cache():
+    a = A()
     # not cached iteration
     start = perf_counter()
-    result_1 = cached_function(1)
+    result_1 = a.cached_function(1, 1)
     time_iteration_1 = perf_counter() - start
     # cached iteration
     start = perf_counter()
-    result_2 = cached_function(1)
+    result_2 = a.cached_function(1, 0)
     time_iteration_2 = perf_counter() - start
     # Use a different cache
     start = perf_counter()
-    cached_function(2)
+    a.cached_function(2, 2)
     time_iteration_3 = perf_counter() - start
 
     assert time_iteration_1 >= time_iteration_2
--- tests/test_ignore_kwargs.py
+++ tests/test_ignore_kwargs.py
@@ -3,26 +3,26 @@ from time import sleep, perf_counter
 from cache_decorator import cache
 
 @cache(
-    cache_path="{cache_dir}/{_hash}.pkl.lzma",
-    cache_dir="./test_cache"
+    cache_dir="./test_cache",
+    args_to_ignore=["x"],
 )
-def cached_function(a):
+def cached_function(a, x):
     sleep(2)
     return [1, 2, 3]
 
 
-def test_pickle_lzma():
+def test_cache():
     # not cached iteration
     start = perf_counter()
-    result_1 = cached_function(1)
+    result_1 = cached_function(1, x=0)
     time_iteration_1 = perf_counter() - start
     # cached iteration
     start = perf_counter()
-    result_2 = cached_function(1)
+    result_2 = cached_function(1, x=1)
     time_iteration_2 = perf_counter() - start
     # Use a different cache
     start = perf_counter()
-    cached_function(2)
+    cached_function(2, x=0)
     time_iteration_3 = perf_counter() - start
 
     assert time_iteration_1 >= time_iteration_2
