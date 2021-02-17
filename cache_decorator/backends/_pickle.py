fac100b5452d85f1866cf21a854f92ba497fc4d8
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-02-17 17:51:18 +0100

    fixed issue #4

--- cache_decorator/backends/_pickle.py
+++ cache_decorator/backends/_pickle.py
@@ -1,20 +1,37 @@
+# Notes:
+# Compress pickle do accept paths as file arguments
+# BUT STANDARD PICKLE DOES NOT.
+
 try:
     from compress_pickle import dump, load
-    extensions = [
-        ".pkl",
-        ".pkl.gz",
-        ".pkl.bz",
-        ".pkl.lzma",
-        ".pkl.zip"
-    ]
-except ModuleNotFoundError:
-    from pickle import dump, load
-    extensions = [".pkl"]
 
-pickle_dict = {
-    extension : {
-        "load":load,
-        "dump":dump
+    pickle_dict = {
+        extension : {
+            "load":load,
+            "dump":dump
+        }
+        for extension in [
+            ".pkl.gz",
+            ".pkl.bz",
+            ".pkl.lzma",
+            ".pkl.zip"
+        ]
     }
-    for extension in extensions
+except ModuleNotFoundError:
+    pickle_dict = {}
+
+from pickle import dump as pickle_dump, load as pickle_load
+extensions = [".pkl"]
+
+def default_dump(value, path):
+    with open(path, "wb") as f:
+        pickle_dump(value, f)
+
+def default_load(path):
+    with open(path, "rb") as f:
+        return pickle_load(f)
+
+pickle_dict[".pkl"] = {
+    "load":default_load,
+    "dump":default_dump
 }
