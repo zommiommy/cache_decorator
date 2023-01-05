79150379aaaf3159db0db801b4c7f7969e3a701a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-05 23:47:52 +0000

    Adding tarfile member sanitization to extractall()

--- cache_decorator/backends/keras_model_backend.py
+++ cache_decorator/backends/keras_model_backend.py
@@ -62,7 +62,26 @@ try:
         def load(self, metadata:dict, path:str) -> object:
             with tempfile.TemporaryDirectory() as tmpdirname:
                 with tarfile.open(path, "r:*") as tar_handle:
-                    tar_handle.extractall(tmpdirname)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner) 
+                        
+                    
+                    safe_extract(tar_handle, tmpdirname)
                 return load_model(tmpdirname, **self._load_kwargs)
 
 # We need to check both for `ModuleNotFoundError`, where
